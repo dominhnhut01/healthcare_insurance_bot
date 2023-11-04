7ac415f6f0a05f895aa9586d296e873cd7220344
dominhnhut01 <dominhnhut01@gmail.com>
2023-11-04 16:41:16 -0400

    completed ChatServer

---
+++ backend/socket.ts
@@ -0,0 +1,50 @@
+import { Server as HttpServer } from "http";
+import { Socket, Server } from "socket.io";
+
+type Color = "white" | "black";
+
+export class ServerSocket {
+  public static instance: ServerSocket;
+  public io: Server;
+
+  /** Master list of all connected rooms */
+
+  constructor(server: HttpServer) {
+    ServerSocket.instance = this;
+    this.io = new Server(server, {
+      serveClient: false,
+      pingInterval: 10000,
+      pingTimeout: 5000,
+      cookie: false,
+      cors: {
+        origin: "*",
+      },
+    });
+
+    this.io.on("connect", this.StartListeners);
+  }
+
+
+  StartListeners = async (socket: Socket) => {
+    console.info("Message received from " + socket.id);
+    console.log("done loading")
+
+
+    socket.on("message", async (message: string) => {
+        this.messageListener(message);
+        this.messageSender(socket, message);
+    })
+
+    socket.on("disconnect", () => {
+      console.info("Disconnect received from: " + socket.id);
+      socket.emit("disconnect")
+    });
+  };
+
+  messageListener(message: string) {
+    console.log(message);
+  }
+  messageSender(socket: Socket, message: string) {
+    socket.emit("message", message);
+  }
+}
