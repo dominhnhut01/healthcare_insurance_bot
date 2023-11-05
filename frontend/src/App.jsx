8ff7535e0ba29fc29bf14be19db396dfdb721421
Loc Huu Vinh Tran <78862643+TranHVLoc@users.noreply.github.com>
2023-11-05 03:01:00 -0500

    24 creating uploading pdf popup (#27)
    * Add popup but bug persists
    
    * temp commit
    
    * fix bug closing backdrop
    
    ---------
    
    Co-authored-by: Loc Tran <loctranforbusiness@gmail.com>
    Co-authored-by: dominhnhut01 <dominhnhut01@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,46 @@
-import { useState } from 'react'
-import { useRoutes, useNavigate } from 'react-router-dom'
-import Upload from './pages/Upload.jsx'
-// import Display from './pages/Display.jsx'
-import ChatContainer from './components/ChatContainer';
+import { useState } from "react";
+import { useRoutes, useNavigate } from "react-router-dom";
+import Upload from "./pages/Upload.jsx";
+import ChatContainer from "./components/ChatContainer";
 
-// import './App.css'
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import Dialog from "@mui/material/Dialog";
+import Backdrop from "@mui/material/Backdrop";
+
+import { socket } from "./socket.js";
 
 function App() {
-  const [file, setFile] = useState(null)
-  const navigate = useNavigate()
+  // file Object
+  const [file, setFile] = useState(null);
+  // open state for dialog
+  const [open, setOpen] = useState(true); // Controls whether the upload dialog is open
+
+  const navigate = useNavigate();
 
   const handleUpload = (uploadFileUrl) => {
-    setFile(uploadFileUrl)
-    navigate('/display')
-  }
+    setFile(uploadFileUrl);
+    console.log(uploadFileUrl);
+    setOpen(false);
+    alert("File uploaded successfully");
+    // navigate('/display')
+  };
 
-  let element = useRoutes([
-    {
-      path: '/',
-      element: <Upload onUpload={handleUpload} />,
-    },
-    {
-      path: '/chat',
-      element: <ChatContainer />,
-    }
-  ])
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <>
-      {element}
+      <ChatContainer />
+      <Backdrop open={open} sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Upload a File</DialogTitle>
+          <DialogContent>
+            <Upload onUpload={handleUpload} />
+          </DialogContent>
+        </Dialog>
+      </Backdrop>
     </>
   );
 }
--- frontend/src/pages/Upload.jsx
+++ frontend/src/pages/Upload.jsx
@@ -1,49 +1,48 @@
-import React, { useState } from 'react';
-import { useDropzone } from 'react-dropzone';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { useNavigate } from "react-router-dom";
+import { socket } from "../socket";
 
-const Upload = () => {
-    const [error, setError] = useState(null);
-    const { getRootProps, getInputProps, acceptedFiles } = useDropzone({
-        accept: 'application/pdf',
-        onDropRejected: () => setError('File upload failed. Please make sure you are uploading a PDF file.'),
-        onDropAccepted: () => setError(null),
-    });
+const Upload = ({ onUpload }) => {
+  const [error, setError] = useState(null);
+  const [file, setFile] = useState(null);
+  const { getRootProps, getInputProps, acceptedFiles } = useDropzone({
+    accept: "application/pdf",
+    onDropRejected: () =>
+      setError(
+        "File upload failed. Please make sure you are uploading a PDF file."
+      ),
+    onDropAccepted: () => setError(null),
+  });
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleUpload = async () => {
-        const file = acceptedFiles[0];
-        if (file) {
-            const formData = new FormData();
-            formData.append('file', file);
+  const handleUpload = async () => {
+    const file = acceptedFiles[0];
+    setFile(file);
+    if (file) {
+      try {
+        socket.emit("upload", file, (status) => {
+          console.log(status.succeed);
+        });
+        onUpload();
+      } catch (error) {
+        console.error("There was an error uploading the file:", error);
+      }
+    } else {
+      setError("No file selected");
+    }
+  };
 
-            try {
-                console.log('Uploading file...', file);
-                const response = await fetch('http://localhost:4800/uploads', {
-                    method: 'POST',
-                    body: formData,
-                });
-                const data = await response.json();
-                console.log(data);  // Log the response from the server
-                // navigate('/display');  // Navigate to the display route
-            } catch (error) {
-                console.error('There was an error uploading the file:', error);
-            }
-        } else {
-            setError('No file selected');
-        }
-    };
-
-    return (
-        <div {...getRootProps()}>
-            {/* <input {...getInputProps()} /> */}
-            <input type="file" {...getInputProps()} name="file" />
-            <p>Drag 'n' drop some files here, or click to select files</p>
-            {error && <p>{error}</p>}
-            <button onClick={handleUpload}>Upload</button>
-        </div>
-    );
+  return (
+    <div {...getRootProps()}>
+      {/* <input {...getInputProps()} /> */}
+      <input type="file" {...getInputProps()} name="file" />
+      <p>Drag 'n' drop some files here, or click to select files</p>
+      {error && <p>{error}</p>}
+      <button onClick={handleUpload}>Upload</button>
+    </div>
+  );
 };
 
 export default Upload;
