ad929883c574932fa7da1e3df474f6d0bf64c38b
dominhnhut01 <dominhnhut01@gmail.com>
2023-11-05 02:52:00 -0500

    fix bug closing backdrop

--- frontend/src/pages/Upload.jsx
+++ frontend/src/pages/Upload.jsx
@@ -1,46 +1,48 @@
-import React, { useState } from 'react';
-import { useDropzone } from 'react-dropzone';
-import { useNavigate } from 'react-router-dom';
-import { socket } from '../socket';
+import { useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { useNavigate } from "react-router-dom";
+import { socket } from "../socket";
 
 const Upload = ({ onUpload }) => {
-    const [error, setError] = useState(null);
-    const { getRootProps, getInputProps, acceptedFiles } = useDropzone({
-        accept: 'application/pdf',
-        onDropRejected: () => setError('File upload failed. Please make sure you are uploading a PDF file.'),
-        onDropAccepted: () => setError(null),
-    });
+  const [error, setError] = useState(null);
+  const [file, setFile] = useState(null);
+  const { getRootProps, getInputProps, acceptedFiles } = useDropzone({
+    accept: "application/pdf",
+    onDropRejected: () =>
+      setError(
+        "File upload failed. Please make sure you are uploading a PDF file."
+      ),
+    onDropAccepted: () => setError(null),
+  });
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleUpload = async () => {
-        const file = acceptedFiles[0];
-        if (file) {
-            
-    
-            try {
-                console.log('Uploading file...', file);
-                socket.emit("upload")
-                const data = await response.json();
-                console.log("here is data", data);  // Log the response from the server
-                onUpload(file);  // Make sure data contains the URL of the uploaded file
-            } catch (error) {
-                console.error('There was an error uploading the file:', error);
-            }
-        } else {
-            setError('No file selected');
-        }
-    };
+  const handleUpload = async () => {
+    const file = acceptedFiles[0];
+    setFile(file);
+    if (file) {
+      try {
+        socket.emit("upload", file, (status) => {
+          console.log(status.succeed);
+        });
+        onUpload();
+      } catch (error) {
+        console.error("There was an error uploading the file:", error);
+      }
+    } else {
+      setError("No file selected");
+    }
+  };
 
-    return (
-        <div {...getRootProps()}>
-            {/* <input {...getInputProps()} /> */}
-            <input type="file" {...getInputProps()} name="file" />
-            <p>Drag 'n' drop some files here, or click to select files</p>
-            {error && <p>{error}</p>}
-            <button onClick={handleUpload}>Upload</button>
-        </div>
-    );
+  return (
+    <div {...getRootProps()}>
+      {/* <input {...getInputProps()} /> */}
+      <input type="file" {...getInputProps()} name="file" />
+      <p>Drag 'n' drop some files here, or click to select files</p>
+      {error && <p>{error}</p>}
+      <button onClick={handleUpload}>Upload</button>
+    </div>
+  );
 };
 
 export default Upload;
